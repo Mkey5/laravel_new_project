7a1e11f05a2a828e6b74974d709e5855cc8e7ad9
Mkey5 <marekradkov@gmail.com>
2016-11-18 19:13:03 +0200

    added cron scheduling for the resources updating

--- laravel_project/app/Console/Kernel.php
+++ laravel_project/app/Console/Kernel.php
@@ -26,6 +26,29 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->call(function(){
+            $allhomeplanets = DB::table('homeplanets')
+                ->join('goldmines','homeplanets.id','=','goldmines.homeplanet_id')
+                ->join('powerplants','homeplanets.id' , '=','powerplants.homeplanet_id')
+                ->join('metalmines','homeplanets.id' , '=','metalmines.homeplanet_id')
+                ->select('homeplanets.id','homeplanets.gold','homeplanets.metal','homeplanets.energy','goldmines.income as gold_income','metalmines.income as metal_income','powerplants.income as energy_income','goldmines.level as goldmines_level','metalmines.level as metalmines_level','powerplants.level as energy_level')
+                ->get();
+
+
+            foreach ($allhomeplanets as $homeplanet) {
+                $homeplanet->gold += ($homeplanet->gold_income) * $homeplanet->goldmines_level;
+                $homeplanet->metal += ($homeplanet->metal_income) * $homeplanet->metalmines_level;
+                $homeplanet->energy += ($homeplanet->energy_income) * $homeplanet->energy_level;
+
+                App\Homeplanet::where('id',$homeplanet->id)->update([
+                    'gold' => $homeplanet->gold,
+                    'metal' => $homeplanet->metal,
+                    'energy' => $homeplanet->energy
+                    ]);
+            }
+
+        })->cron('2 * * * * *'); // every 2 minutes will add income to homeplanet resources
     }
 
     /**
